2d4da78f98b8848eca7d5217d729b3d281d97707
Krispin Noya <66666443+noyakrispin@users.noreply.github.com>
2024-05-18 22:05:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 def test(temp_input):
-    set_endpoints = temp_input[2].strip('()').split(',')
+    set_endpoints = extract_data(temp_input[2])
     set_endpoints_dict = {key: True for key in set_endpoints}
-    connections = temp_input[1].strip('()').split(',')
+    connections = extract_data(temp_input[1])
     result = []
     for connection in connections:
-        first_endpoint = connection[0]
-        second_endpoint = connection[2]
+        first_endpoint, second_endpoint = extract_endpoints(connection)
         if not set_endpoints_dict.get(first_endpoint) and not set_endpoints_dict.get(second_endpoint):
             result.append(connection)
 
     print(result if len(result) else 'yes')
 
 
+def extract_data(data):
+    return data.strip('()').split(',')
+
+
+def extract_endpoints(connection):
+    return connection[0], connection[2]
+
+
 if __name__ == '__main__':
     temp_input = ['(A,B,C,D)', '(A-B,B-C,C-D)', '(A,C)']
     test(temp_input)
-
